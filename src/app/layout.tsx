ed3bf8b9811a51943b4515fb21c91661dc8aa438
Angel Lopez Pol <59091763+angel1254mc@users.noreply.github.com>
2023-10-08 14:49:21 -0400

    Initialize Project Setup (#2)
    * initial project setup w/ create-next-app
    
    * Remove Next.js boilerplate
    
    * Configure ESLint/Prettier
    
    * Configure SSG/Deployment
    
    * Move eslint to devDependencies

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Society of Software Developers",
+  description: "Student organization for Software Development and Design.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Society of Software Developers</h1>
+    </main>
+  );
+}
