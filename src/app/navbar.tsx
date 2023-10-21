d7ef2fae8b945480f2c8ce0f18d6b8c2a3c681e6
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-10-21 14:54:55 -0400

    Add basic navbar styles and social link

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -6,11 +6,27 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <header className={styles.navbar}>
-      <Image src="/ssd_logo.svg" alt="SSD Logo" width="70" height="70" />
+      <Link href="/">
+        <Image src="/ssd_logo.svg" alt="SSD Logo" width="50" height="50" />
+      </Link>
       <nav>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
       </nav>
+      <div className={styles.socialLinks}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://discord.gg/5SyB3yx"
+        >
+          <Image
+            src="/discord_mark.svg"
+            alt="Discord Logo"
+            width="40"
+            height="40"
+          />
+        </a>
+      </div>
     </header>
   );
 }
